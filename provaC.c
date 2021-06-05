eac4481aab7c18ffe27386a4b7236ae638fde9c7
markustnt <76594613+markustnt@users.noreply.github.com>
2021-06-05 14:29:29 +0200

    Add files via upload

---
+++ provaC.c
@@ -0,0 +1,22 @@
+ /*markustnt*/
+ 
+ /*
+     Nella dichiarazione dell variabili:
+     Bisogna prestare attenzione alle MAIUSCOLE e alle minuscole
+     Non è possibile inserire degli spazi
+     Una variabile non può iniziare con un numero
+     Non può avere il nome di istruzioni o funzioni in C
+     
+ */
+ 
+#include<stdio.h>
+
+main()
+{
+	
+	int num1 = 8;
+	int num2;
+	num2 = 4;
+	printf("%d \n", num1);
+	printf("%d", num2);
+}
