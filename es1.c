b937ee87a20d5a635fe2597022a57d7cb8a524b0
Marco Mondello <76594613+markustnt@users.noreply.github.com>
2021-06-06 20:12:06 +0200

    Create es1.c
    Questo programma permette all'utente di inserire un numero, per poi visualizzarlo incrementato di 1.

---
+++ es1.c
@@ -0,0 +1,18 @@
+ /*markustnt*/
+ 
+//Programma che incrementa il valore del numero immesso di 1
+
+ 
+#include <stdio.h>
+main()
+{
+	
+	int a, b; 
+	
+	printf("Inserisci il numero: "); 
+	scanf("%d", &a); 
+	
+	b= a + 1; 
+	printf("\nIl nuovo numero: ");
+	printf("%d", b);
+}
